6f02588cf9d3aa56ae63e2a153f0b21a78308e7f
Judah Hamaoui <ayhamaoui@gmail.com>
2017-03-10 18:29:23 -0500

    synced our models

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
+//const morgan = require('morgan');
 const nunjucks = require('nunjucks')
+const router = require('./routes/index.js')
+const models = require('./models');
 
-app.use(morgan('combined'));
+//app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
+app.use('/', router)
+
+
 
 var env = nunjucks.configure('views', {noCache: true}); // have res.render work with html files
 app.set('view engine', 'html');                         // when res.render works with html files, have it use nunjucks to do so
 app.engine('html', nunjucks.render);
 
-app.listen(3000, function () {
-	console.log('Express started on http://localhost:' + 3000);
-});
+
+models.User.sync({})
+.then(function () {
+    return models.Page.sync({})
+})
+.then(function () {
+    app.listen(3000, function () {
+        console.log('Server is listening on port 3000!');
+    });
+})
+.catch(console.error);
+
+// app.listen(3000, function () {
+// 	console.log('Express started on http://localhost:' + 3000);
+// });
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', function(req,res){
+    console.log('I\'m ALIIIIVE!!')
+    res.send('I\'m still alive')
+})
+
 module.exports = router;
